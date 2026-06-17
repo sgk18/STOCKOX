235cf9277bffcc2ee1b4279788bbeca95069466e
Suryachalam V M <suryachalam18@gmail.com>
2026-06-17 10:19:58 +0530

    feat: initialize project with custom ESLint configuration and custom rule overrides

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -13,6 +13,15 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/no-unescaped-entities": "off"
+    }
+  }
 ]);
 
 export default eslintConfig;
